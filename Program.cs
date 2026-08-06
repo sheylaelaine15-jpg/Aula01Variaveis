ec4ab47cd8b355e616bafa772b43a7e4598e17ea
Sheyla e Larissa <sheylaelaine15@gmail.com>
2026-08-05 22:27:21 -0300

    Aula 01/02 Váriaveis e Métodos

---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+
+namespace Aula01Variaveis
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           //ConcatenarPalavras(); 
+           //CalcularMedia();
+           //CalcularTabuada();
+           VerificarAulaEtec();
+           
+        }
+
+        public static void VerificarAulaEtec()
+        {
+            Console.WriteLine("Digite a data");
+            DateTime data = DateTime.Parse(Console.ReadLine());
+
+            if (data.DayOfWeek == DayOfWeek.Saturday || data.DayOfWeek == DayOfWeek.Sunday)
+            {
+                Console.WriteLine("Final de semana! Hoje não tem aula! Revisarei exercícios.");
+            }
+            else
+            {
+                Console.WriteLine("Dia da semana! Bora pra Etec!");
+            }
+        }
+
+        public static void CalcularTabuada()
+        {
+            Console.WriteLine("Digite a Tabuada que deseja");
+            int Tabuada = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            while (contador <= 10)
+            {
+                string mensagem = string.Format("{0} X {1} = {2}", Tabuada, contador, Tabuada * contador);
+
+                Console.WriteLine(mensagem);
+                contador++;
+            }
+        }
+
+        
+        public static void CalcularMedia()
+        {
+            Console.WriteLine("Digite a primeira nota");
+            decimal nota1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a segunda nota");
+            decimal nota2 = decimal.Parse(Console.ReadLine());
+
+            decimal media = (nota1 + nota2) / 2;
+            Console.WriteLine($"A média é {media}");
+
+            if (media >= 7)
+                Console.WriteLine("Aprovado");
+            else if (media < 7 && media >= 4)
+                Console.WriteLine("Recuperação");
+            else
+                Console.WriteLine("Reprovado");
+        }
+
+
+        public static void ConcatenarPalavras()
+        {
+
+             Console.WriteLine("Digite o seu nome");
+            string nome = Console.ReadLine();
+
+            string frase1 = $"Olá {nome}, hoje é {DateTime.Now}";
+            Console.WriteLine(frase1);
+            Console.WriteLine("==============================");
+
+            Console.WriteLine("Quanto custa um dólar em reais?");
+            decimal valorDolarReais = decimal.Parse(Console.ReadLine());
+
+            string frase2 = string.Format(
+                "Hoje é {0:dd/MM/yyyy}, o dólar está custando {1:c2}",
+                DateTime.Now,
+                valorDolarReais
+            );
+
+            Console.WriteLine(frase2);
+            Console.WriteLine("==============================");
+
+            string cabecalho = string.Format(
+                "{0:dddd}, {0:dd} de {0:MMMM} de {0:yy} - {0:HH:mm:ss}",
+                DateTime.Now
+            );
+
+            Console.WriteLine(cabecalho);
+        }
+    }
+}
+
